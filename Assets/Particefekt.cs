6a2044c69ea6b6f81b1c11ef6eb6088c33e0e8ff
bcgkky <bcgkky1@gmail.com>
2023-12-07 12:48:19 +0300

    last
    ...

---
+++ Assets/Particefekt.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particefekt : MonoBehaviour
+{
+    ParticleSystem parct;
+
+    void Start()
+    {
+        parct = GetComponent<ParticleSystem>();
+
+    }
+
+    void OnParticleTrigger()
+    {
+        print("elleme");
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        if (!other.CompareTag("ParTag"))
+        {
+            Destroy(other.gameObject);
+            print(other.gameObject.name);
+        }
+    }
+    void OnParticleSystemStopped()
+    {
+        print("efettin bitti brom.");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            parct.Stop();
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            parct.Play();
+        }
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            parct.Pause();
+        }
+
+    }
+}
